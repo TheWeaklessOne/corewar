ea4c6aaa51936ecf3d8baf7422dd6433100ad04f
Diedra Joye <djoye@ox-l5.21-school.ru>
2020-01-20 15:53:38 +0300

    color

--- vm/print.c
+++ vm/print.c
@@ -6,7 +6,7 @@
 /*   By: djoye <djoye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 18:24:30 by djoye             #+#    #+#             */
-/*   Updated: 2020/01/18 20:32:27 by djoye            ###   ########.fr       */
+/*   Updated: 2020/01/20 15:50:32 by djoye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	print_arena_test(t_vm *vm, int step)
 	int			row;
 	int			column;
 	int			color;
-	int			cycle;
+	int			cycle;	
 	
     initscr(); // перемещение курсора в стандартном экране y=10 x=30
 	keypad(stdscr, true);
@@ -54,31 +54,54 @@ void	print_arena_test(t_vm *vm, int step)
     	   speed > 100 ? speed /= 2 : 0;
 		column = 0;
 		row = 1;
+		start_color();
+		init_pair(0, COLOR_WHITE, COLOR_BLACK);
+		init_pair(1, COLOR_RED, COLOR_BLACK);
+		init_pair(2, COLOR_GREEN, COLOR_BLACK);
+		init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+		init_pair(4, COLOR_BLUE, COLOR_BLACK);
 		while(++i < MEM_SIZE)
         {
-			//use_default_colors();
-			//start_color();
-			color = vm->map_color[i] == 0 ? 7 : vm->map_color[i];
-			wattron(vm_window, color);
-			//init_pair(1, color, COLOR_BLACK);
-			//wbkgdset(vm_window, COLOR_PAIR(1));
-			mvwprintw(vm_window, row, column += 3 , "%02x", vm->arena[i]);
-			vm->arena[i] +=  vm->arena[i] != 0 ? 1 : 0; // test
-			if (i != 0 && (i + 1) % 64 == 0)
-			{
-				row++;
-				column = 0;
-			}
-			//wrefresh(vm_window);
+			if (i % 64 == 0)
+				wmove(vm_window, row++, 2);
+			color = vm->map_color[i] == 0 ? 0 : vm->map_color[i];
+			wattron(vm_window, COLOR_PAIR(color));
+			wprintw(vm_window, "%02x", vm->arena[i]);
+			wattroff(vm_window, COLOR_PAIR(color));
+			waddch(vm_window, ' ');
+			vm->arena[i] +=  vm->arena[i] != 0 ? color : 0; // test
 		}
-		mvwprintw(vm_window, 3, 200, "cycles = %d", cycle);
-		mvwprintw(vm_window, 4, 200, "speed = %d", speed);
-		mvwprintw(vm_window, 5, 200, "players = %d", vm->players);
-		mvwprintw(vm_window, 6, 200, "champs = %d", vm->champ[0]);
-		mvwprintw(vm_window, 8, 200, "cycle_to_die = %d", vm->champ);
-		mvwprintw(vm_window, 7, 200, "champs = %d", vm->champ[1]);
+		
+	//	wattron(vm_window, COLOR_RED);
+	
+		
+//init_pair(1, color, COLOR_BLACK);
+			//wbkgdset(vm_window, COLOR_PAIR(1));
+	//wbkgdset(vm_window, COLOR_PAIR(1));
+/*
+ 50
+ 0
+Cursors : 2
+Player -1 : Mikael Blomkvist
+Last live :                   0
+Lives in current period :     0
+
+*/
+		mvwprintw(vm_window, 3, 200, "Cycles/second : %d", 1000 / speed);
+		mvwprintw(vm_window, 4, 200, "Cycle : %d", cycle);
+		mvwprintw(vm_window, 5, 200, "Cursors : %d", vm->curs);
+
+		mvwprintw(vm_window, 6, 200, "players = %d", vm->players);
+		mvwprintw(vm_window, 7, 200, "champs = %d", vm->champ[0]);
+		mvwprintw(vm_window, 8, 200, "Cycle to die : %d", CYCLE_TO_DIE);
+		mvwprintw(vm_window, 9, 200, "Cycle delta : %d", CYCLE_DELTA);
+		mvwprintw(vm_window, 10, 200, "Lives : %d/%d", vm->champ, NBR_LIVE);
+		mvwprintw(vm_window, 11, 200, "Cycles to check : %d", vm->champ);
+		mvwprintw(vm_window, 12, 200, "Checks : %d/%d", vm->champ[1], MAX_CHECKS);
+		//wattroff(vm_window, COLOR_RED);
 		wrefresh(vm_window);
 		usleep(speed);
+		
 	}
 	wclear(vm_window);
     endwin(); // завершение работы с ncurses
