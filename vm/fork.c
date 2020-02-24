da596467efccf581007c6fada2ed1db4cc6b494f
anisa121 <anisa56695@gmail.com>
2020-02-24 17:00:14 +0300

    fork lfork

--- vm/fork.c
+++ vm/fork.c
@@ -42,17 +42,21 @@ void			do_fork(t_vm *vm, t_cur *cur)
 	new = create_cursor(cur);
 	if (-(cur->args[0]) > cur->pos)
 	{
+		(vm->l == 1) ? ft_printf("fork %d (%d)\n", cur->args[0],
+								 ((cur->pos + cur->args[0] % IDX_MOD))) : 0;
 		cur->args[0] %= IDX_MOD;
 		cur->args[0] += cur->pos;
 		new->pos = MEM_SIZE + cur->args[0];
 	}
 	else
+	{
+		(vm->l == 1) ? ft_printf("fork %d (%d)\n", cur->args[0],
+								 ((cur->pos + cur->args[0] % IDX_MOD))) : 0;
 		new->pos = (cur->pos + cur->args[0] % IDX_MOD) % MEM_SIZE;
+	}
 	new->next = vm->curs;
 	vm->curs = new;
 	vm->curs_alive++;
-	(vm->l == 1) ? ft_printf("fork %d (%d)\n", cur->args[0],
-			((cur->pos + cur->args[0] % IDX_MOD))) : 0;
 }
 
 void			do_lfork(t_vm *vm, t_cur *cur)
@@ -64,15 +68,19 @@ void			do_lfork(t_vm *vm, t_cur *cur)
 	new = create_cursor(cur);
 	if (-(cur->args[0]) > cur->pos)
 	{
+		(vm->l == 1) ? ft_printf("lfork %d (%d)\n", cur->args[0],
+								 (cur->pos + cur->args[0])) : 0;
 		cur->args[0] += cur->pos;
 		cur->args[0] %= MEM_SIZE;
 		new->pos = MEM_SIZE + cur->args[0];
 	}
 	else
+	{
+		(vm->l == 1) ? ft_printf("lfork %d (%d)\n", cur->args[0],
+								 (cur->pos + cur->args[0])) : 0;
 		new->pos = (cur->pos + cur->args[0]) % MEM_SIZE;
+	}
 	new->next = vm->curs;
 	vm->curs = new;
 	vm->curs_alive++;
-	(vm->l == 1) ? ft_printf("lfork %d (%d)\n", cur->args[0],
-			(cur->pos + cur->args[0])) : 0;
 }
