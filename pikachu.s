19f4b6a8ee9b5a131387d1c05b8772a31354cfbf
anisa121 <anisa56695@gmail.com>
2020-02-24 18:46:39 +0300

    our champ

---
+++ pikachu.s
@@ -0,0 +1,19 @@
+.name "pikachu PAWUK"
+.comment "how dare you??????"
+
+code:	live %42
+		sti  r15,%0,r14
+		zjmp %-200
+		live %42
+		sti  r15,%0,r14
+		zjmp %-200
+		live %42
+		sti  r15,%0,r14
+		zjmp %-200
+		live %42
+		sti  r15,%0,r14
+		zjmp %-200
+		sti  r15,%0,r14
+		zjmp %-200
+		sti  r15,%0,r14
+		zjmp %-200
